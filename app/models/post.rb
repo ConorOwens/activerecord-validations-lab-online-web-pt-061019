c6a62f47ecdbf737474f4317750fb7bd05b3685d
Conor Owens <conorj.owens@gmail.com>
2019-11-03 23:07:27 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ActiveRecord::Base
   validate :clickbait
   
   def clickbait
-    if title.exclude? ("Won't Believe") && title.exclude? ("Secret") && title.exclude? ("Won't Believe") && title.exclude? ("Won't Believe") && title.exclude? ("Won't Believe") &&
+    if title.exclude? ("Won't Believe") && title.exclude? ("Secret") && title.exclude? ("Top [") && title.exclude? ("Guess")
   
 end
